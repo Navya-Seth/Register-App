fb3e8936ac064b9132023a4517a4a505d0ede266
Navya-Seth <91605093+Navya-Seth@users.noreply.github.com>
2022-05-11 16:29:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
 var studentarray=[];
 
 function submit(){
-    var name1=document.getElementById("student 1").value;
-    var name2=document.getElementById("student 2").value;
-    var name3=document.getElementById("student 3").value;
-    var name4=document.getElementById("student 4").value;
-    var name5=document.getElementById("student 5").value;
-    studentarray.push(name1);
-    studentarray.push(name2);
-    studentarray.push(name3);
-    studentarray.push(name4);
-    studentarray.push(name5);
+    var display_student_array=[];
+    for (var j=1; j <=5; j++){
+        var name_of_student= document.getElementById("student "+j).value;
+        console.log(name_of_student);
+        studentarray.push(name_of_student);
+    }
+
     console.log(studentarray);
-    document.getElementById("display").innerHTML=studentarray;
-    document.getElementById("submit").style.display="none";
+    var length_of_studentarray=studentarray.length;
+    console.log(length_of_studentarray);
+    for (var n=0; n <length_of_studentarray; n++){
+        display_student_array.push("<h4>name ="+studentarray[n]+"</h4>")
+        console.log(display_student_array);
+    }
+
+    console.log(display_student_array);
+    document.getElementById("displaywith").innerHTML=display_student_array;
+    var remove=display_student_array.join("  ");
+    console.log(remove);
+    document.getElementById("displaywithout").innerHTML=remove;
+
+           document.getElementById("submit").style.display="none";
     document.getElementById("sort").style.display="inline-block";
 }
 
 function sorting(){
     studentarray.sort();
     console.log(studentarray);
-    document.getElementById("display").innerHTML=studentarray;
+    var display_sortting=[];
+    var length_of_studentarray=studentarray.length;
+    console.log(length_of_studentarray);
+    for (var n=0; n <length_of_studentarray; n++){
+        display_sortting.push("<h4>name ="+studentarray[n]+"</h4>")
+        console.log(display_sortting);
+    }
+    var remove=display_student_array.join("  ");
+    console.log(remove);
+    document.getElementById("displaywithout").innerHTML=remove;
+ 
 }
